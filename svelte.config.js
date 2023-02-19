b0f3ee40407bdc09d413bfec2a6c2f6c75fc2b6a
dhruv <hi@dhrv.pw>
2023-02-19 12:07:17 +0530

    ci: add vercel-adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,6 +8,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
+		// NOTE: using adapter vercel https://kit.svelte.dev/docs/adapter-vercel
 		adapter: adapter()
 	}
 };
