43d6245ecbd7bbc434fa6ae6f9c11d6d084efe43
dhruv <hi@dhrv.pw>
2023-02-22 01:36:58 +0530

    feat: add font settings

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,4 @@
-<h1>[in-progress]</h1>
+<section class="w-full min-h-screen flex justify-between items-center">
+	<h1 class=" font-norma text-2xl">dhrv.page</h1>
+	<div class="w-8 h-8 mb-4 bg-red-500 rounded-full" />
+</section>
